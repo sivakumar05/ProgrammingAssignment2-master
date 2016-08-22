77bbfb426435d0ca3ea659a7b13523970e791e6a
sivakumar05 <sivakumar.menta@gmail.com>
2016-08-22 11:48:41 +0530

    Add files via upload

---
+++ cachematrix.R
@@ -0,0 +1,35 @@
+## I want to compute Inverse of a matrix ; when inputs are unchanged & inverse is computed ,I want to cache result
+## functions do
+
+## This function creates a special "matrix" object that can cache its inverse
+
+makeCacheMatrix <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
+
+
+
+
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
+
+cachesolve <- function(x, ...) {
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+}
